d72df00489868ae9471fa0a61f914c21538733ec
tarun <tarun@TarunAgarwal-ThinkPad>
2018-09-19 13:15:59 +0530

    Test commit

--- PreSpringSample-01/src/main/java/com/denofprogramming/service/MessagePrinterService.java
+++ PreSpringSample-01/src/main/java/com/denofprogramming/service/MessagePrinterService.java
@@ -3,14 +3,14 @@ package com.denofprogramming.service;
 
 public class MessagePrinterService {
    
-    private final BasicMessageOfTheDayImpl service = new BasicMessageOfTheDayImpl();
-
+    //private final BasicMessageOfTheDayImpl service = new BasicMessageOfTheDayImpl();
+    private final DynamicMessageOfTheDayImpl service2 = new DynamicMessageOfTheDayImpl();
     
     public MessagePrinterService(){    	
     }
     
     public void printMessage() {
-        System.out.println(service.getTextMessage());
+        System.out.println(service2.getMessage());
     }
 	
 }
